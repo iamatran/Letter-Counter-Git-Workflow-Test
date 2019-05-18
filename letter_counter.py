604bd39871a2b859f582540753fb7f2ca4be529f
iamatran <iamatran@hotmail.com>
2019-05-18 01:04:33 -0500

    added counting letters
    to add enhancement #2

--- letter_counter.py
+++ letter_counter.py
@@ -1,13 +1,16 @@
 def main():
     print("This will count the # of vowels in your phrase")
+    
+    lettersToCount = input("Enter letters to count: ").lower()
     inputPhrase = input("Enter phrase: ").lower()
 
-    totalVowels = 0
-    for character in inputPhrase:
-        if character in ['a', 'e', 'i', 'o', 'u']:
-            totalVowels = totalVowels + 1
+    totalOccurrencesOfLettersToCount = 0
 
-    print("Total vowels: {}".format(totalVowels))
+    for character in inputPhrase:
+        if character in lettersToCount:
+            totalOccurrencesOfLettersToCount = totalOccurrencesOfLettersToCount + 1
+
+    print("Total occurrences of '{}' in your phrase: {}".format(lettersToCount, totalOccurrencesOfLettersToCount))
 
 if __name__ == "__main__":
     main()
